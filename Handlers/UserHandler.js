d7d013d5f7a8b1ecfbc7d8ee7095055393996862
EmileDeGamer <47303713+EmileDeGamer@users.noreply.github.com>
2021-09-29 11:29:16 +0200

    Fixed bad user feedback at login and registration

--- Handlers/UserHandler.js
+++ Handlers/UserHandler.js
@@ -49,7 +49,7 @@ module.exports = function HandleUser(socket){
         if(data.name != null){
             const nameRegex = /^[a-z ,.'-]+$/i
             if(!nameRegex.test(data.name)){
-                errors.push('Name doesn\'t match regex')
+                errors.push(`Name can't contain numbers`)
             }
         }
         else{
@@ -58,7 +58,7 @@ module.exports = function HandleUser(socket){
         if(data.username != null){
             const usernameRegex = /\w{5,29}/i
             if(!usernameRegex.test(data.username)){
-                errors.push('Username doesn\'t match regex')
+                errors.push('Username must be between 5 and 29 characters long')
             }
         }
         else{
@@ -67,7 +67,7 @@ module.exports = function HandleUser(socket){
         if(data.email != null){
             const emailRegex = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/
             if(!emailRegex.test(data.email)){
-                errors.push('Email doesn\'t match regex')
+                errors.push(`Email must contain an '@'`)
             }
         }
         else{
@@ -76,7 +76,7 @@ module.exports = function HandleUser(socket){
         if(data.password != null){
             const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/
             if(!passwordRegex.test(data.password)){
-                errors.push('Password doesn\'t match regex')
+                errors.push('Password must be atleast 8 characters long, contain an special character and 1 number and 1 uppercase')
             }
         }
         else{
@@ -85,7 +85,7 @@ module.exports = function HandleUser(socket){
         if(data.confirmPassword != null){
             const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/
             if(!passwordRegex.test(data.confirmPassword) && data.password != data.confirmPassword){
-                errors.push('Passwords doesn\'t match eachother')
+                errors.push('Passwords must be the same')
             }
         }
         else{
