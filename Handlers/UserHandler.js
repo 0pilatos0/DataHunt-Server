192766f51f150757789bf393557c96f741958e76
EmileDeGamer <47303713+EmileDeGamer@users.noreply.github.com>
2021-10-05 19:44:31 +0200

    made email regex dry

--- Handlers/UserHandler.js
+++ Handlers/UserHandler.js
@@ -1,5 +1,6 @@
 const Mailer = require("../Core/Mailer")
-const { Salter } = require("../Core/Salter")
+const Regex = require("../Core/Regex")
+const Salter = require("../Core/Salter")
 const HTMLLoader = require("../Loaders/HTMLLoader")
 const User = require("../Models/User")
 
@@ -65,8 +66,7 @@ module.exports = function HandleUser(socket){
             errors.push('Username must be filled in')
         }
         if(data.email != null){
-            const emailRegex = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/
-            if(!emailRegex.test(data.email)){
+            if(!Regex.Email.test(data.email)){
                 errors.push(`Email must contain an '@'`)
             }
         }
@@ -138,8 +138,7 @@ module.exports = function HandleUser(socket){
         let errors = []
         let success = []
         if(data.email != null){
-            const emailRegex = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/
-            if(!emailRegex.test(data.email)){
+            if(!Regex.Email.test(data.email)){
                 errors.push(`Email must contain an '@'`)
             }
         }
@@ -170,7 +169,7 @@ module.exports = function HandleUser(socket){
                     }
                 })
 
-                //TODO send mail with reset link  + token
+                //TODO send mail with reset link + token
 
                 success.push("You now received an email with a reset link")
             } else{
