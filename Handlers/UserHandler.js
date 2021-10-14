eac08b0245d711f8718f87e5b72ad62e4b3f6c42
EmileDeGamer <47303713+EmileDeGamer@users.noreply.github.com>
2021-10-14 16:54:35 +0200

    fixed repeat password check bug

--- Handlers/UserHandler.js
+++ Handlers/UserHandler.js
@@ -82,7 +82,7 @@ module.exports = function HandleUser(socket){
             errors.push('Password must be filled in')
         }
         if(data.confirmPassword != null){
-            if(!Regex.Password.test(data.confirmPassword) && data.password != data.confirmPassword){
+            if(Regex.Password.test(data.confirmPassword) && data.password != data.confirmPassword){
                 errors.push('Passwords must be the same')
             }
         }
