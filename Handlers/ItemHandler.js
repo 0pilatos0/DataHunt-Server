5d800671ce7438bacc3f23ed7ee9f8d719dfd674
Sjaak Kant <kantsjaak@gmail.com>
2021-11-01 11:37:09 +0100

    finished gets for inventory

--- Handlers/ItemHandler.js
+++ Handlers/ItemHandler.js
@@ -2,8 +2,7 @@ const Item = require("../Models/Item")
 const Inventory = require("../Models/Inventory")
 
 module.exports = function HandleItem(socket) {
-    socket.on('getInventory', async (id) => {
-        let inventoryData = await Inventory.Get(7)
+    socket.on('getInventory', async (id) => {let inventoryData = await Inventory.Get(7)
         inventoryData = JSON.parse(inventoryData.json)
         const returnData =  new Object
         let amount=0
@@ -12,40 +11,22 @@ module.exports = function HandleItem(socket) {
              if (Object.prototype.hasOwnProperty.call(inventoryData, property)) {
                 returnData[property] = []
                 for (let entry = 0; entry < Object.entries(inventoryData[property]).length; entry++) {
-                    returnData[property].push(entry.id, entry.value)
+                    let data = await Item.GetInventoryItem(inventoryData[property][entry].id)
+                    let temp = {}
+
+                    Object.keys(data).forEach(result => {
+                        temp[result] = data[result]
+                    });
+
+                    temp.id = inventoryData[property][entry].id
+                    temp.value = inventoryData[property][entry].value
+                    returnData[property].push(temp)
                 }
              }
              if(amount == Object.entries(inventoryData).length){
-                console.log('YOOOOOOOOOOoo')
+                socket.emit("returnedInventory", returnData)
             }
-         }
-
-         console.log(returnData)
-
-
-        // Object.entries(inventoryData).map(async entry => {
-        //     let counter = 0;
-        //     entry[1].forEach(async element => {
-        //         console.log(entry[1])
-        //         let temp = await Item.GetInventoryItem(element.id)
-
-        //         for (var property in element) {
-        //             if (Object.prototype.hasOwnProperty.call(element, property)) {
-        //                 temp[property] = element[property]
-        //             }
-        //             //var temp heeft data hier 
-        //             if (Object.keys(temp).length === 7) {
-        //                 console.log(Object.keys(temp).length)
-        //                 returnData.push(temp)
-        //             }
-        //         }
-        //         counter++
-        //         //var temp is leeg hier
-        //         if (entry[1].length == counter) {
-        //             console.log(returnData)
-        //             socket.emit("returnedInventory", returnData)
-        //         }
-        //     });
-        // })
+        }
+        
     })
 }
