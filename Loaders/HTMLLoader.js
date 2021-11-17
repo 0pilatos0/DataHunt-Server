4a72fff4c9fdfbe9c83a2ad93fb629b3b995971f
kantsjaak@gmail.com <kantsjaak@gmail.com>
2021-11-17 12:04:21 +0100

    Documentation done - database related things

--- Loaders/HTMLLoader.js
+++ Loaders/HTMLLoader.js
@@ -4,7 +4,12 @@ module.exports = class HTMLLoader{
     constructor() {
         
     }
-
+    
+    /**
+     * Loads the HTML file
+     * @param {String} path
+     * @returns {String}
+     */
     static Read(path){
         return fs.readFileSync(path, {encoding:'utf8', flag:'r'})
     }
